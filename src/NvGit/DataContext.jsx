55d188c367e15d7e072405f7b46282a289490eb5
mers-argueta <146381143+mers-argueta@users.noreply.github.com>
2025-02-03 20:49:55 -0600

    Add files via upload

---
+++ src/NvGit/DataContext.jsx
@@ -0,0 +1,8 @@
+export const dataUsuarios = [
+    {
+      name: "Esperando programadores",
+      github: "https://github.com",
+      imgSrc: "https://avatars.githubusercontent.com/u/82402877?v=4",
+    },
+  ];
+  
